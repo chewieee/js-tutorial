3aba44fce06dedb5b554ea38a440a086bd9afa68
Matt <ichewmatt@gmail.com>
2020-05-17 15:26:56 -0700

    add translatedphrase object

--- palindrome.js
+++ palindrome.js
@@ -6,10 +6,34 @@ function reverse(string) {
 
 // Defines a Phrase Object
 function Phrase(content) {
-	this.content = content;
+  this.content = content;
 
-	this.palindrome = function palindrome() {
-	  let processedContent = this.content.toLowerCase();
-	  return processedContent === reverse(processedContent);
-  };
+  this.processor = function processor(string) {
+  	return string.toLowerCase();
+  }
+
+  //Returns content process for palindrom testing
+  this.processedContent = function processedContent() {
+  	return this.processor(this.content);
+  }
+  
+  this.palindrome = function palindrome() {
+	return this.processedContent() === reverse(this.processedContent());
+  }
+
+  this.louder = function louder() {
+  	return this.content.toUpperCase();
+  }
 }
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+	this.content = content;
+	this.translation = translation;
+
+	this.processedContent = function processedContent() {
+  	  return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
