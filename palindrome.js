99e56bff0bc463561f2c88483715ac8f300e8a28
Matt <ichewmatt@gmail.com>
2020-05-17 15:51:09 -0700

    tweak a few functions

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,15 @@
-// Reverses a string
-function reverse(string) {
-	return Array.from(string).reverse().join("");
+// Adds reverse to all strings
+String.prototype.reverse = function() {
+	return Array.from(this).reverse().join("");
 }
 
+String.prototype.blank = function() {
+	return !!(this.match(/^\s*$/g));
+}
 
+Array.prototype.last = function() {
+	return this[this.length - 1];
+}
 // Defines a Phrase Object
 function Phrase(content) {
   this.content = content;
